47d4bc3ccd69e249f6c6094be0b879e42b045ad8
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-11-07 08:27:58 +0530

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import type { Metadata } from "next";
 
 import AuthProvider from "@/providers/auth.provider";
+import ServiceWorkerRegistration from "@/components/ServiceWorkerRegistration";
 
 import { Toaster } from "@/components/ui/sonner";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -20,7 +21,13 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Lyceum AI Admin",
+  manifest: "/manifest.json",
   description: "Admin panel for managing Lyceum AI",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "lyceumai admin",
+  },
 };
 
 export default function RootLayout({
@@ -30,11 +37,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased select-none`}
       >
         <AuthProvider>{children}</AuthProvider>
         <Toaster />
+        <ServiceWorkerRegistration />
       </body>
     </html>
   );
---
+++ app/offline/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OfflinePage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <h1>You are offline</h1>
+      <p>Please check your internet connection</p>
+    </div>
+  );
+};
+
+export default OfflinePage;
